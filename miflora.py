2aa239a82f2cffefc7ae31dfc421903019da238c
tamasjambor <jamborta@gmail.com>
2016-11-30 13:50:38 +0000

    skip discovery

--- miflora.py
+++ miflora.py
@@ -13,42 +13,43 @@ from gattlib import DiscoveryService, GATTRequester, GATTResponse
 
 verbose = True
 
-service = DiscoveryService("hci0")
+# service = DiscoveryService("hci0")
 # if asking for root, try restarting service (hciconfig hci0 down, hciconfig hci0 up)
 # to make this work downgrade the firmware (rpi2) sudo rpi-update 33a6707cf1c96b8a2b5dac2ac9dead590db9fcaa
-devices = service.discover(15)
+# devices = service.discover(15)
+
+a = ["C4:7C:8D:61:95:E9", "C4:7C:8D:61:99:B3", "C4:7C:8D:61:92:49"]
 
 baseTopic = "openhab/miflower/"
 msgs = []
 
-for address, name in list(devices.items()):
+for address, name in a:
 	try:
-		if (name == "Flower care"):
-			topic = baseTopic + address.replace(':', '') + '/'
-			requester = GATTRequester(address, True)
-			# Read battery and firmware version attribute
-			data = requester.read_by_handle(0x0038)[0]
-			battery, firmware = unpack('<B6s', data)
-			msgs.append({'topic': topic + 'battery', 'payload': battery})
-			msgs.append({'topic': topic + 'firmware', 'payload': firmware})
-			# Enable real-time data reading
-			requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
-			# Read plant data
-			data = requester.read_by_handle(0x0035)[0]
-			requester.disconnect()
-			temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx', data)
-			msgs.append({'topic': topic + 'temperature', 'payload': temperature / 10.})
-			msgs.append({'topic': topic + 'sunlight', 'payload': sunlight})
-			msgs.append({'topic': topic + 'moisture', 'payload': moisture})
-			msgs.append({'topic': topic + 'fertility', 'payload': fertility})
-			if (verbose):
-				print("name: {}, address: {}".format(name, address))
-				print "Battery level:", battery, "%"
-				print "Firmware version:", firmware
-				print "Light intensity:", sunlight, "lux"
-				print "Temperature:", temperature / 10., " C"
-				print "Soil moisture:", moisture, "%"
-				print "Soil fertility:", fertility, "uS/cm"
+		topic = baseTopic + address.replace(':', '') + '/'
+		requester = GATTRequester(address, True)
+		# Read battery and firmware version attribute
+		data = requester.read_by_handle(0x0038)[0]
+		battery, firmware = unpack('<B6s', data)
+		msgs.append({'topic': topic + 'battery', 'payload': battery})
+		msgs.append({'topic': topic + 'firmware', 'payload': firmware})
+		# Enable real-time data reading
+		requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
+		# Read plant data
+		data = requester.read_by_handle(0x0035)[0]
+		requester.disconnect()
+		temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx', data)
+		msgs.append({'topic': topic + 'temperature', 'payload': temperature / 10.})
+		msgs.append({'topic': topic + 'sunlight', 'payload': sunlight})
+		msgs.append({'topic': topic + 'moisture', 'payload': moisture})
+		msgs.append({'topic': topic + 'fertility', 'payload': fertility})
+		if (verbose):
+			print("name: {}, address: {}".format(name, address))
+			print "Battery level:", battery, "%"
+			print "Firmware version:", firmware
+			print "Light intensity:", sunlight, "lux"
+			print "Temperature:", temperature / 10., " C"
+			print "Soil moisture:", moisture, "%"
+			print "Soil fertility:", fertility, "uS/cm"
 	except:
 		print "Error during reading:", sys.exc_info()[0]
 
