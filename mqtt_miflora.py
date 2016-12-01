a24625d64332fcb688a8fa77651616fe05fe9558
tamasjambor <jamborta@gmail.com>
2016-12-01 16:18:25 +0000

    run forever

--- mqtt_miflora.py
+++ mqtt_miflora.py
@@ -17,31 +17,33 @@ plants = [plant1, plant2, plant3]
 baseTopic = "openhab/miflower/"
 msgs = []
 
-for plant in plants:
+while True:
 
-	try:
-		print("Getting data from Mi Flora")
-		print("MAC address: {}".format(plant._mac))
-		print("FW: {}".format(plant.firmware_version()))
-		print("Name: {}".format(plant.name()))
-		print("Temperature: {} C".format(plant.parameter_value(MI_TEMPERATURE)))
-		print("Moisture: {}".format(plant.parameter_value(MI_MOISTURE)))
-		print("Light: {} lux".format(plant.parameter_value(MI_LIGHT)))
-		print("Conductivity: {} uS/cm".format(plant.parameter_value(MI_CONDUCTIVITY)))
-		print("Battery: {} %".format(plant.parameter_value(MI_BATTERY)))
+	for plant in plants:
 
-		topic = baseTopic + plant._mac + '/'
-		# Read battery and firmware version attribute
-		msgs.append({'topic': topic + 'battery', 'payload': plant.parameter_value(MI_BATTERY)})
-		msgs.append({'topic': topic + 'firmware', 'payload': plant.firmware_version()})
-		msgs.append({'topic': topic + 'temperature', 'payload': plant.parameter_value(MI_TEMPERATURE)})
-		msgs.append({'topic': topic + 'light', 'payload': plant.parameter_value(MI_LIGHT)})
-		msgs.append({'topic': topic + 'moisture', 'payload': plant.parameter_value(MI_MOISTURE)})
-		msgs.append({'topic': topic + 'fertility', 'payload': plant.parameter_value(MI_CONDUCTIVITY)})
-	except:
-		print("Error during reading:", sys.exc_info()[0])
+		try:
+			print("Getting data from Mi Flora")
+			print("MAC address: {}".format(plant._mac))
+			print("FW: {}".format(plant.firmware_version()))
+			print("Name: {}".format(plant.name()))
+			print("Temperature: {} C".format(plant.parameter_value(MI_TEMPERATURE)))
+			print("Moisture: {}".format(plant.parameter_value(MI_MOISTURE)))
+			print("Light: {} lux".format(plant.parameter_value(MI_LIGHT)))
+			print("Conductivity: {} uS/cm".format(plant.parameter_value(MI_CONDUCTIVITY)))
+			print("Battery: {} %".format(plant.parameter_value(MI_BATTERY)))
 
-	if len(msgs) > 0:
-		publish.multiple(msgs, hostname="localhost", port=1883, keepalive=60, will=None, auth=None, tls=None)
+			topic = baseTopic + plant._mac + '/'
+			# Read battery and firmware version attribute
+			msgs.append({'topic': topic + 'battery', 'payload': plant.parameter_value(MI_BATTERY)})
+			msgs.append({'topic': topic + 'firmware', 'payload': plant.firmware_version()})
+			msgs.append({'topic': topic + 'temperature', 'payload': plant.parameter_value(MI_TEMPERATURE)})
+			msgs.append({'topic': topic + 'light', 'payload': plant.parameter_value(MI_LIGHT)})
+			msgs.append({'topic': topic + 'moisture', 'payload': plant.parameter_value(MI_MOISTURE)})
+			msgs.append({'topic': topic + 'fertility', 'payload': plant.parameter_value(MI_CONDUCTIVITY)})
+		except:
+			print("Error during reading:", sys.exc_info()[0])
+
+		if len(msgs) > 0:
+			publish.multiple(msgs, hostname="localhost", port=1883, keepalive=60, will=None, auth=None, tls=None)
 
 	time.sleep(60)
